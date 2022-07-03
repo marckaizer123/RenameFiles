7120f86f212f79f5ea11d5f1dec354ce91f6b25e
Marc Lorenz Menguito <c17-2807-458@uphsl.edu.ph>
2022-07-03 16:11:31 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace RenameFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\Marc\Downloads\Unsorted Documents\Rename");
+            FileInfo[] files = directory.GetFiles().OrderBy(f => f.LastWriteTime).ToArray();
+            Console.WriteLine("Enter the shared name of your files: ");
+            string input = Console.ReadLine();
+            int count = 1;
+            string fileExtension = "";
+            DirectoryInfo directory2 =  System.IO.Directory.CreateDirectory(directory.FullName + "\\Module 1 - " + input);
+
+
+            foreach (FileInfo file in files)
+            {
+                fileExtension = file.Extension;
+
+                if (count < 10)
+                    File.Move(file.FullName, directory2.FullName + "\\" + input + " 0" + count.ToString() + fileExtension, true);
+                else
+                    File.Move(file.FullName, directory2.FullName + "\\" + input + " " + count.ToString() + fileExtension, true);
+
+                count++;
+            }
+            
+
+
+        }
+    }
+}
